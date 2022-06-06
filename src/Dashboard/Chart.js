36b4ccd64ba37fd24c38c90328dac6be7eaf2ec6
fleetimeee <novian.andika@students.amikom.ac.id>
2022-06-06 11:43:08 +0700

    ad

--- src/Dashboard/Chart.js
+++ src/Dashboard/Chart.js
@@ -1,6 +1,4 @@
 import * as React from "react";
-import { useState, useEffect } from "react";
-import axios from "axios";
 
 import Title from "./Title";
 import { Typography } from "@mui/material";
